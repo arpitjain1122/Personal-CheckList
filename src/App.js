7cf53319c787facdcf980702303444c057c22151
arpit jain <jain.arpit@northeastern.edu>
2025-03-23 11:59:10 -0500

    checklist

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import AddItem from "./components/AddItem";
+import SearchItem from "./components/SearchItem";
+
+function App() {
+  const API_URL = "http://localhost:3500/items";
+
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchitems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error("did not recieve expected data");
+        const listItems = await response.json();
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    /*we are here simulating the actual scenarioe where we in real world we will get API api response after some time. in the mean time till the data loads we want to show some test like "Loading data..." and them load the data. this is very useful */
+    setTimeout(() => {
+      fetchitems();
+    }, 2000);
+  }, []);
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setItems(listItems);
+  };
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem("");
+  };
+
+  return (
+    <div className="App">
+      <Header title="Grocery List" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem search={search} setSearch={setSearch} />
+      <main>
+        {isLoading && <p>Loading items...</p>}
+        {fetchError && <p>{`Error : ${fetchError}`}</p>}
+        {!fetchError && !isLoading && (
+          <Content
+            items={items.filter((item) =>
+              item.item.toLowerCase().includes(search.toLowerCase())
+            )}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
+        )}
+      </main>
+      <Footer length={items.length} />
+    </div>
+  );
+}
+
+export default App;
+
+// Command to see the data using json server on the browser
+//  npx json-server -p 3500 -w data/db.json
